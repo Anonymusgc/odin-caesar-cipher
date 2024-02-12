91fb55ddedd9c26e2fcd8d914de6bb05c0e553bb
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-02-12 17:56:21 +0100

    Refactor code to fit rubocop guidelines

--- scrpit.rb
+++ scrpit.rb
@@ -1,15 +1,22 @@
+# frozen_string_literal: true
+
+def char_in_range(char, shift)
+  char_num = char.ord
+  char_after_shift = (char_num + shift)
+  case char_num
+  when 97..122
+    char_after_shift > 122 ? (96 + (char_after_shift - 122)).chr : char_after_shift.chr
+  when 65..90
+    char_after_shift > 90 ? (64 + (char_after_shift - 90)).chr : char_after_shift.chr
+  else
+    char
+  end
+end
+
 def caesar_cipher(string, shift)
   chars = string.split('')
   arr = chars.map do |char|
-    char_num = char.ord
-    case char_num
-    when 97..122
-      (char_num + shift) > 122 ? (96 + ((char_num + shift) - 122)).chr : (char_num + shift).chr
-    when 65..90
-      (char_num + shift) > 90 ? (64 + ((char_num + shift) - 90)).chr : (char_num + shift).chr
-    else
-      char
-    end
+    char_in_range(char, shift)
   end
   puts arr.join('')
 end
