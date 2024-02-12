4cfe22f1d56c12ce7b4aeef4543ad1f71e2c459d
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-02-12 17:16:13 +0100

    Rewrite caesar cipher to change only letter

--- scrpit.rb
+++ scrpit.rb
@@ -1,10 +1,20 @@
 def caesar_cipher(string, shift)
-  word_arr = string.split(" ")
-  arr =
-    word_arr.map do |word|
-      word.split("").map { |letter| (letter.ord + shift).chr }.join("")
+  chars = string.split('')
+  arr = chars.map do |char|
+    char_num = char.ord
+    case char_num
+    when 97..122
+      (char_num + shift).chr
+    when 65..90
+      (char_num + shift).chr
+    else
+      char
     end
-  p arr.join(" ")
+  end.join('')
+  # arr = word_arr.map do |word|
+  #   word.split('').map { |letter| (letter.ord + shift).chr }.join('')
+  # end.join(' ')
+  p arr
 end
 
-caesar_cipher("A random sentence", 1)
+caesar_cipher('A random sentence!', 1)
